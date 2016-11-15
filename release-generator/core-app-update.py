20570115a81e8d8bcd896517495e8e1ec08d9b16
Keiichiro Ono <keiono@gmail.com>
2016-11-15 15:43:51 -0800

    Release script updated for 3.5.0 RC1.

---
+++ release-generator/core-app-update.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulStoneSoup
+import re
+import csv
+
+def replaceVer(node):
+    exe = node.executions.find_all('execution')
+    for e in exe:
+        if e.id.text == 'copy':
+            items = e.configuration.artifactItems.find_all('artifactItem')
+            for item in items:
+                app_name = item.artifactId.text
+                if app_name == 'cy-rest':
+                    # Special case: cyREST
+                    app_name = 'cyREST'
+                item.version.string.replace_with(ver_map[app_name])
+
+# Read versions from output of shell script
+VER_FILE = './cy-release-work-dir/apps/versions.txt'
+ver_map = {}
+
+with open(VER_FILE, 'r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        ver_map[row[0]] = row[1]
+
+print(ver_map)
+
+XMLFILE = './cy-release-work-dir/cytoscape/gui-distribution/assembly/pom.xml'
+
+f = open(XMLFILE, 'r')
+soup = BeautifulStoneSoup(f.read())
+f.close()
+
+res = soup.build.plugins.find_all('plugin')
+print(type(res))
+
+for r in res:
+    p = r.artifactId
+    if p.text == 'maven-dependency-plugin':
+        replaceVer(r)
+
+with open(XMLFILE, "w+b") as file:
+    file.write(soup.prettify('utf-8', formatter='xml'))
---
+++ release-generator/create-release.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+################################################################################
+#
+# Release generator for Cytocape with core apps.
+#
+# - Requirments:
+#    - bash v4.x
+#    - python 3.x
+#    - install4j v6 (for the last step)
+#
+# by Keiichiro Ono
+#
+################################################################################
+
+# New branch for release
+BRANCH="release/3.5.0"
+
+# Current development version
+CURRENT="3.5.0-SNAPSHOT"
+
+# New release version (for the core, not core apps!)
+TARGET="3.5.0-RC1"
+
+# List of all core modules
+REPOSITORIES=(parent api support impl gui-distribution app-developer)
+
+# List of all core
+CORE_APPS=(biopax command-dialog core-apps-meta cyREST datasource-biogrid \
+json idmapper network-analyzer network-merge opencl-cycl opencl-layout \
+psi-mi sbml welcome webservice-psicquic-client webservice-biomart-client)
+
+# List of files to be updated in this release process
+original_dir=`pwd`
+
+# Get the top-level dir
+git clone git@github.com:cytoscape/cytoscape.git
+mv ./cytoscape ./cy-release-work-dir
+cd ./cy-release-work-dir
+
+# Clone repos
+./cy.sh init
+./cy.sh apps
+
+########## Prepare new core apps ##########
+
+# Create branch for apps
+cd ./apps
+last_dir=`pwd`
+
+declare -A app_versions
+
+for app in "${CORE_APPS[@]}"; do
+  cd $app
+  echo "- Switching to ${BRANCH}: $app"
+  git checkout -b $BRANCH || { echo Could not checkout branch $BRANCH; }
+	sed -E -e "s/-SNAPSHOT<\/version>/<\/version>/g" pom.xml > pom.updated.xml
+	mv pom.updated.xml pom.xml
+  # mvn clean install || { echo Failed to build new version: $app; }
+  res=$(printf 'VERSION=${project.version}\n0\n'\
+   | mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
+   | grep '^VERSION' \
+   | sed -E -e "s/VERSION=//g")
+
+  app_versions["${app}"]=$res
+  echo "$app,$res" >> ../versions.txt
+
+  # This is for updating children
+  mvn versions:update-child-modules
+  mvn versions:commit
+  mvn clean install || { echo Failed to build $app; exit 1;}
+
+  # Now commit the change to local repo.
+  git add -A
+  git commit -m "Version number updated to ${res} for Cytoscape release ${TARGET}."
+  cd ..
+done
+
+# Update the gui-distribution/assembly/pom.xml
+cd $original_dir
+python ./core-app-update.py
+
+############## Next, update Cytoscape core modules #################
+cd $original_dir
+
+# Update verison
+cd ./cy-release-work-dir/cytoscape
+cytoscape_dir=`pwd`
+
+# Create new release branch
+git checkout -b $BRANCH || { echo Could not checkout branch $BRANCH; exit 1;}
+for repo in "${REPOSITORIES[@]}"; do
+  cd $repo
+  echo "# Target repo: $repo"
+  git checkout -b $BRANCH || { echo Could not checkout branch $BRANCH; exit 1;}
+  cd ..
+done
+
+# Batch update (for version numbers)
+cd ${cytoscape_dir}
+echo "* Updating version numbers..."
+find . -name pom.xml | xargs sed -i "" -e "s/${CURRENT}/${TARGET}/g"
+
+# Update misc. files
+cd gui-distribution/assembly/src/main/bin
+sed -E -e "s/-SNAPSHOT//g" cytoscape.sh > cytoscape-up.sh
+mv cytoscape-up.sh cytoscape.sh
+sed -E -e "s/-SNAPSHOT//g" cytoscape.bat > cytoscape-up.bat
+mv cytoscape-up.bat cytoscape.bat
+cd ${cytoscape_dir}
+
+# Build everything
+mvn clean install || { echo Failed to build Cytoscape; exit 1;}
+cd gui-distribution/packaging
+mvn clean install || { echo Failed to create installers; exit 1;}
+
+cd ${cytoscape_dir}
+# Commit changes
+for repo in "${REPOSITORIES[@]}"; do
+  cd $repo
+
+  git add -A
+  git commit -m "Version number updated for ${TARGET}."
+  cd ..
+done
+
+git add -A
+git commit -m "Version number updated for ${TARGET}."
+
+
+echo "-------------- Done! ---------------"
+echo "! Don't forget to re-format gui-distribution/assembly/pom.xml"
