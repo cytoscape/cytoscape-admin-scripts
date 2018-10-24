f3ebd7c1c7bc31abd383508c71c3ef194e64f172
Brett Settle <brettjsettle@gmail.com>
2018-10-24 07:35:29 -0700

    Updated for 3.7.0 release

--- system-checker/windows.bat
+++ system-checker/windows.bat
@@ -3,7 +3,7 @@
 REM This is a handy reference for this scripting language: https://technet.microsoft.com/en-us/library/cc754335(v=ws.11).aspx
 
 REM Target Cytoscape version for this script
-set CYTOSCAPE_VERSION=3.6.0
+set CYTOSCAPE_VERSION=3.7.0
 
 REM Cytoscpae App Store location
 set APP_STORE_URL=apps.cytoscape.org
