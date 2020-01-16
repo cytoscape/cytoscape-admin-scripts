a5d082f2c5b289a84a84a189ae2b54c909045b30
William Markuske <wmarkuske@ucsd.edu>
2020-01-16 08:16:53 -0800

    Add Catalina to supported OS
    Added macOS 10.15 Catalina to list of supported mac versions.

--- system-checker/mac.sh
+++ system-checker/mac.sh
@@ -8,7 +8,7 @@
 CYTOSCAPE_VERSION="3.7.2"
 
 # Supported Mac OS versions
-SUPPORTED_OS_VERSIONS=("10.11" "10.12" "10.13" "10.14")
+SUPPORTED_OS_VERSIONS=("10.11" "10.12" "10.13" "10.14" "10.15")
 
 # Supported Java verisons
 SUPPORTED_JAVA_VERSIONS=("1.8.0")
