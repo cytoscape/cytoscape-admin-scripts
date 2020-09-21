5aa2921dcdb0cc71a629ff4929483d4813d71381
Alexander Pico <apico@gladstone.ucsf.edu>
2020-09-21 16:32:05 -0700

    minor tweaks to subset

--- project-stats/publications/CytoscapePubStats.R
+++ project-stats/publications/CytoscapePubStats.R
@@ -12,10 +12,16 @@ cytoscape_search <- europepmc::epmc_search(query='cytoscape')
 ##Update query with limit based on how many are available. Note this will be very slow.
 cytoscape_search <- europepmc::epmc_search(query='cytoscape', limit=21247)
 
-##Make subset
-columns <- c("pmid", "journalTitle", "citedByCount")
-stats <- cytoscape_search[columns]
+##Make subsets
+stats <- cytoscape_search %>%
+  select(c("pmid", "journalTitle", "citedByCount")) %>%
+  drop_na()
 
+### papers sorted by citations
+citation_count <- stats %>%
+  arrange(desc(citedByCount))
+
+### journals sorted by paper counts
 journal_count <- stats %>%
   group_by(journalTitle) %>%
   summarise(count = n()) %>%
