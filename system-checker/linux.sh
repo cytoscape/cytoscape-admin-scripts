6f6be8db1227bddd6ddd2ef8825af0b3cd72ecb1
Chris Churas <churas.camera@gmail.com>
2024-08-22 17:25:03 -0700

    updated to 3.10.2

--- system-checker/linux.sh
+++ system-checker/linux.sh
@@ -4,7 +4,7 @@
 ####################################
 
 # Target Cytoscape version for this script
-CYTOSCAPE_VERSION="3.10.1"
+CYTOSCAPE_VERSION="3.10.2"
 
 # Cytoscpae App Store location
 APP_STORE_URL="apps.cytoscape.org"
